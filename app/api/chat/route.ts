0e95e53ec9b0a2ca977472d4dfdcbb259cfc0f3b
Prathyusha <prathyushaballa91@gmail.com>
2025-10-30 00:02:08 -0600

    First Deployment

---
+++ app/api/chat/route.ts
@@ -0,0 +1,25 @@
+import { google } from '@ai-sdk/google';
+import { streamText, UIMessage, convertToModelMessages } from 'ai';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages }: { messages: UIMessage[] } = await req.json();
+
+  const result = streamText({
+    model: google('gemini-2.5-flash'),
+    tools: {
+      google_search: google.tools.googleSearch({}),
+    },
+    messages: convertToModelMessages(messages),
+  });
+  const providerMetadata = await result.providerMetadata;
+  type GoogleGenerativeAIProviderMetadata = { groundingMetadata?: unknown };
+  const metadata = providerMetadata?.google as
+  | GoogleGenerativeAIProviderMetadata
+  | undefined;
+const groundingMetadata = metadata?.groundingMetadata;
+console.log(groundingMetadata,JSON.stringify(groundingMetadata,null,2));
+  return result.toUIMessageStreamResponse();
+}
